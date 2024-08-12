bef497ad1838d9f113e2f3b8f6d3595bb40c7743
ShivamSachdeva12 <sachdevashivam506@gmail.com>
2024-08-13 02:13:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+from preprocess import preprocess_text
+import nltk
+nltk.download('punkt_tab')
+
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = preprocess_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
---
+++ preprocess.py
@@ -0,0 +1,54 @@
+import string
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
+
+import re
+
+
+# Download necessary NLTK resources
+nltk.download('punkt')
+
+
+def preprocess_text(text):
+    # 1. Convert all characters to lowercase
+    text = text.lower()
+
+    # 2. Remove URLs
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+
+    # 3. Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # 4. Remove stopwords
+    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
+
+    text = ''.join(char if char.isalpha() or char.isspace() else '' for char in text)
+    # Optional: Remove extra spaces
+    text = ' '.join(text.split())
+
+    words = text.split()
+    new_text = []
+    for word in words:
+        # Check if the word is in the abbreviation dictionary
+        word_lower = word.lower()
+        slangs = pd.read_csv("Abbreviations and Slang.csv")
+        # Convert DataFrame to dictionary
+        abbreviation_dict = pd.Series(slangs.Text.values, index=slangs.Abbreviations).to_dict()
+        if word_lower in abbreviation_dict:
+            # Replace it with the full form
+            new_text.append(abbreviation_dict[word_lower])
+        else:
+            # Keep the word as is
+            new_text.append(word)
+    text = ' '.join(new_text)
+
+    tokens = word_tokenize(text)
+    stemmer = PorterStemmer()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    text = ' '.join(stemmed_tokens)
+
+    return text
